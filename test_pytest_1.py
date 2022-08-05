658bd68496286f075f4bd54d22e7d9faee15898f
LoganF-1234 <logan.ferrera@meritagehomes.com>
2022-08-05 09:06:27 -0700

    first commit

---
+++ test_pytest_1.py
@@ -0,0 +1,23 @@
+import pytest
+import page as page
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+from time import sleep
+import sys
+
+
+@pytest.mark.usefixtures("driver_chrome_init")
+class BasicTest:
+    pass
+
+class Test_URL_Chrome(BasicTest):
+  def test_header_arizona(self):
+    self.driver.get('https://www.meritagehomes.com/')
+    self.driver.set_window_position(0,0)
+    self.driver.set_window_size(1920,1080)
+    home_page = page.MainPage(self.driver)
+    home_page.click_state("az")
+    title = "Arizona"
+    assert title == self.driver.title
