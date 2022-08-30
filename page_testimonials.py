3558a5e74d6efd039e920d6c3cff4dbef498341b
LoganF-1234 <logan.ferrera@meritagehomes.com>
2022-08-30 10:01:56 -0700

    Testimonial tests

--- page_testimonials.py
+++ page_testimonials.py
@@ -73,3 +73,76 @@ class TestimonialsPage(BasePage):
   def click_element_row_play_button(self, number):
     row = self.get_element_section_1_row(number)
     return self.click_element_section_play_button(row)
+  
+  def get_element_aside_2(self):
+    return self.driver.find_element(By.XPATH, "/html/body/main/aside[2]")
+  
+  def get_text_avid_header(self):
+    aside = self.get_element_aside_2()
+    return self.get_text_section_header(aside)
+  
+  def get_text_avid_body(self):
+    aside = self.get_element_aside_2()
+    return self.get_text_section_body(aside)
+  
+  def get_element_avid_image(self):
+    aside = self.get_element_aside_2()
+    return self.get_element_section_placeholder_image(aside)
+  
+  def click_element_avid_button(self):
+    aside = self.get_element_aside_2()
+    self.click_element_section_button(aside)
+  
+  def get_element_section_local_lowdown(self):
+    return self.driver.find_element(By.ID, 'testimonial-dropdown-display')
+
+  def get_text_section_local_header(self):
+    section = self.get_element_section_local_lowdown()
+    return self.get_text_section_header(section)
+  
+  def get_text_section_local_body(self):
+    section = self.get_element_section_local_lowdown()
+    return self.get_text_section_body(section)
+  
+  def click_element_section_local_dropdown(self):
+    section = self.get_element_section_local_lowdown()
+    dropdown = section.find_element(By.XPATH, "/html/body/main/article[2]/div/div/div[2]/div/label/select")
+    dropdown.click()
+  
+  def click_element_section_local_dropdown_select_a_metro(self):
+    section = self.get_element_section_local_lowdown()
+    select_a_metro_option = section.find_element(By.XPATH, '/html/body/main/article[2]/div/div/div[2]/div/label/select/option')
+    self.click_element_section_local_dropdown()
+    select_a_metro_option.click()
+    
+  def get_elements_section_local_under_dropdown(self):
+    section = self.get_element_section_local_lowdown()
+    parent_div = section.find_element(By.XPATH, '/html/body/main/article[2]/div/div/div[3]')
+    homeowner_quotes = parent_div.find_elements(By.TAG_NAME, 'div')
+    return homeowner_quotes
+
+  def get_element_section_testimonial_submission(self):
+    return self.driver.find_element(By.ID, 'form-area')
+
+  def get_text_testimonial_submission_header(self):
+    submission_form = self.get_element_section_testimonial_submission()
+    return self.get_text_section_header(submission_form)
+  
+  def get_text_testimonial_submission_body(self):
+    submission_form = self.get_element_section_testimonial_submission()
+    return self.get_text_section_body(submission_form)
+  
+  def get_element_aside_3(self):
+    return self.driver.find_element(By.XPATH, '/html/body/main/aside[3]')
+  
+  def get_text_aside_3_header(self):
+    aside_3 = self.get_element_aside_3()
+    return self.get_text_section_header(aside_3)
+  
+  def get_text_aside_3_body(self):
+    aside_3 = self.get_element_aside_3()
+    return self.get_text_section_body(aside_3)
+  
+  def click_element_aside_3_button(self):
+    aside_3 = self.get_element_aside_3()
+    self.click_element_section_button(aside_3)
--- test_testimonials_page.py
+++ test_testimonials_page.py
@@ -24,13 +24,13 @@ class Test_Testimonials_Page(BasicTest):
     assert "Alex & Catherine" in header
 
   # Test whether the correct text is displayed in main sub_header
-  def test_main_sub_header(self):
+  def test_main_sub_header(self, driver_settings):
     test_page = TestimonialsPage(self.driver)
     sub_header = test_page.get_text_main_sub_header()
     assert "Alex and Catherine describe" in sub_header
   
   # Test whether the plauy button opens youtube overlay on click
-  def test_main_video_button(self):
+  def test_main_video_button(self, driver_settings):
     test_page = TestimonialsPage(self.driver)
     test_page.click_element_play_button()
     youtube_overlay = test_page.get_element_youtube_overlay()
@@ -44,14 +44,15 @@ class Test_Testimonials_Page(BasicTest):
     assert "A slice" in header
   
   # Test whether the correct text is displayed in aside 1 body
-  def test_aside_1_body(self):
+  def test_aside_1_body(self, driver_settings):
     test_page = TestimonialsPage(self.driver)
     body = test_page.get_text_aside_1_body()
     assert "the place we yearn for" in body
   
   # Test whether the aside 1 button navigates to correct page on click
-  def test_aside_1_button(self):
+  def test_aside_1_button(self, driver_settings):
     test_page = TestimonialsPage(self.driver)
+    test_page.close_cookies()
     test_page.click_element_aside_1_button()
     assert "" == self.driver.title
   
@@ -112,7 +113,6 @@ class Test_Testimonials_Page(BasicTest):
     assert result
   
   # Test whether the play buttons on the testimonial rows open the yotubue ovelray when pressed
-  @pytest.mark.testim
   @pytest.mark.parametrize('row', [1, 2, 3, 4, 5, 6])
   def test_row_overlay_on_play_button_press(self, row, driver_settings):
     test_page = TestimonialsPage(self.driver)
@@ -121,3 +121,103 @@ class Test_Testimonials_Page(BasicTest):
     youtube_overlay = test_page.get_element_youtube_overlay()
     result = self.driver.execute_script("return arguments[0].style.display != \"none\"", youtube_overlay)
     assert result
+  
+  # Test that the correct header is displayed in aside 2
+  def test_aside_2_header(self, driver_settings):
+    test_page = TestimonialsPage(self.driver)
+    header = test_page.get_text_avid_header()
+    assert "Get the local lowdown" in header
+  
+  # Test that the correct body text is displayed in aside 2
+  def test_aside_2_body(self, driver_settings):
+    test_page = TestimonialsPage(self.driver)
+    body = test_page.get_text_avid_body()
+    assert "Select your state to see what homeowners" in body
+  
+  # Test that the correct image is displayed in aside 2
+  def test_aside_2_image(self, driver_settings):
+    test_page = TestimonialsPage(self.driver)
+    image = test_page.get_element_avid_image()
+    result = self.driver.execute_script("return arguments[0].complete && " + "arguments[0].width > 0", image)
+    assert result
+  
+  # Test that the button in aside 2 navigates to correct page when clicked
+  # THIS LINK IS BROKEN
+  def test_aside_2_button(self, driver_settings):
+    test_page = TestimonialsPage(self.driver)
+    test_page.close_cookies()
+    test_page.click_element_avid_button()
+    assert "Page not found" in self.driver.title
+  
+  # Test that the correct header is displayed for the local lowdown section/article
+  def test_section_local_lowdown_header(self, driver_settings):
+    test_page = TestimonialsPage(self.driver)
+    header = test_page.get_text_section_local_header()
+    assert "Get the local lowdown" in header
+  
+  # Test that the correct body text is displayed in section local lowdown
+  def test_section_local_lowdown_body(self, driver_settings):
+    test_page = TestimonialsPage(self.driver)
+    body = test_page.get_text_section_local_body()
+    assert "Select your state to see what homeowners" in body
+  
+  # Test that clicking on a metro area in the dropdown displays a dynamic number of homeowner testimonials depending on region selected. To test this, first make sure that the default option is displaying quotes, then select "select a metro area" from the dropdown and test that there are no quotes displayed
+  def test_section_local_lowdown_dropdwon(self, driver_settings):
+    test_page = TestimonialsPage(self.driver)
+    test_page.close_cookies()
+    
+    # For option Phoenix selected
+    phoenix = False
+    phoenix_homeowner_testimonials = test_page.get_elements_section_local_under_dropdown()
+    if len(phoenix_homeowner_testimonials) > 0:
+      phoenix = True
+    
+    #For "Select a metro area" selected
+    select_metro = False
+    test_page.click_element_section_local_dropdown()
+    test_page.click_element_section_local_dropdown_select_a_metro()
+    select_homeowner_testimonials = test_page.get_elements_section_local_under_dropdown()
+    if len(select_homeowner_testimonials) == 0:
+      select_metro = True
+    
+    assert phoenix and select_metro
+  
+  # Test whether correct header is displayed for the testimonial submission section
+  def test_testimonial_submission_header(self, driver_settings):
+    test_page = TestimonialsPage(self.driver)
+    header = test_page.get_text_testimonial_submission_header()
+    assert "Share your story" in header
+
+  # Test whether correct body text is displayed for the testimonial submission section
+  def test_testimonial_submission_body(self, driver_settings):
+    test_page = TestimonialsPage(self.driver)
+    body = test_page.get_text_testimonial_submission_body()
+    assert "Are you a Meritage homeowner with a noteworthy story to tell" in body
+  
+  # Tests for the share your story form
+  ###############
+  
+  ###############
+
+  # Test whether correct header is displayed in aside 3
+  def test_aside_3_header(self, driver_settings):
+    test_page = TestimonialsPage(self.driver)
+    header = test_page.get_text_aside_3_header()
+    assert "Your story begins here" in header
+  
+  # Test whether correct body is displayed in aside 3
+  def test_aside_3_body(self, driver_settings):
+    test_page = TestimonialsPage(self.driver)
+    body = test_page.get_text_aside_3_body()
+    assert "There are many great homes" in body
+
+  # Test whether button in aside 3 navigates to correct page on click
+  def test_aside_3_button(self, driver_settings):
+    test_page = TestimonialsPage(self.driver)
+    test_page.close_cookies()
+    test_page.click_element_aside_3_button()
+    assert "Find a Home | Meritage Homes" == self.driver.title
+
+  # Fails on testimonial submission header
+  # and tesimonial_submission body
+  # and test_aside_2_button
