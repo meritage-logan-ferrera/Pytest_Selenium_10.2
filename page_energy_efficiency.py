c16ddca2b7505032cba114308766a95b199c655b
LoganF-1234 <logan.ferrera@meritagehomes.com>
2022-09-07 12:44:01 -0700

    Tests up to HERS completed Need to see if they need to be more extensive

--- page_energy_efficiency.py
+++ page_energy_efficiency.py
@@ -102,8 +102,131 @@ class EnergyEfficiencyPage(BasePage):
     return current_slide.find_element(By.TAG_NAME, 'p').text
     
   def click_element_donut_section_right_arrow(self):
-    self.driver.find_element(By.XPATH, '/html/body/main/div[1]/div[1]/div/div/div/div[2]/div/div/button[2]').click()
-  
-  def get_elements_donut_current_slide_buttons(self):
     current_slide = self.get_element_donut_section_current_slick_slide()
-    return current_slide.find_elements(By.TAG_NAME, 'li')
+    button = self.driver.find_element(By.XPATH, '/html/body/main/div[1]/div[1]/div/div/div/div[2]/div/div/button[2]')
+    button.click()
+    WebDriverWait(self.driver, timeout=5).until(EC.element_to_be_clickable(button))
+  
+  def click_element_current_slide_button(self, number):
+    current_slide = self.get_element_donut_section_current_slick_slide()
+    header = self.get_text_donut_section_slide_header()
+    slide_number = 2
+    if "MORE SAVINGS" in header:
+      print("slide number is", slide_number)
+    if "BETTER HEALTH" in header:
+      slide_number = slide_number + 1
+      print('slide number is: ', slide_number)
+    if "REAL COMFORT" in header:
+      slide_number = slide_number + 2
+      print('slide number is: ', slide_number)
+    if "PEACE OF MIND" in header:
+      slide_number = slide_number + 3
+      print('slide number is: ', slide_number)
+
+    button = self.driver.find_element(By.XPATH, f'/html/body/main/div[1]/div[1]/div/div/div/div[2]/div/ul[1]/div/div/li[{slide_number}]/div[2]/ul/li[{number}]/a')
+    self.driver.execute_script('arguments[0].scrollIntoView(true); window.scrollBy(0, -300)', button)
+    button.click()
+  
+  def get_element_current_slide_button_popup(self, number):
+    current_slide = self.get_element_donut_section_current_slick_slide()
+    final_number = number
+    header = self.get_text_donut_section_slide_header()
+    if "MORE SAVINGS" in header:
+      print("final number is", final_number)
+    if "BETTER HEALTH" in header:
+      final_number = number + 4
+      print('final number is: ', final_number)
+    if "REAL COMFORT" in header:
+      final_number = number + 8
+      print('final number is: ', final_number)
+    if "PEACE OF MIND" in header:
+      final_number = number + 11
+      print('final number is: ', final_number)
+
+    return self.driver.find_element(By.XPATH, f'/html/body/main/div[1]/div[1]/div/div/div/div[2]/div/ul[2]/li[{final_number}]')
+  
+  def click_element_empty_space(self):
+    self.driver.find_element(By.XPATH, '/html/body/main/article[1]/div/div[2]/div').click()
+  
+  def get_text_house_icon_section_header(self):
+    return self.driver.find_element(By.XPATH, '/html/body/main/div[1]/div[2]/div[1]/p/strong').text
+  
+  def get_element_house_icon_section_image(self):
+    return self.driver.find_element(By.ID, 'ee-cutaway--image-map')
+
+  def get_elements_house_icons(self):
+    icons = self.driver.find_elements(By.XPATH, '/html/body/main/div[1]/div[2]/div[2]/div/div/ul//li')
+    return icons
+  
+  def check_icons_is_clickable(self, icon_number):
+    icon = self.driver.find_element(By.XPATH, f'/html/body/main/div[1]/div[2]/div[2]/div/div/ul/li[{icon_number}]')
+    bool = False
+    bool = WebDriverWait(self.driver, timeout=3).until(EC.element_to_be_clickable(icon))
+    return bool
+  
+  def get_text_gallery_slider_section_header(self):
+    return self.driver.find_element(By.XPATH, '/html/body/main/div[1]/div[3]/div/div/p').text
+  
+  def get_text_gallery_slider_section_sub_header(self):
+    return self.driver.find_element(By.XPATH, '/html/body/main/div[1]/div[3]/div/div/p/strong').text
+  
+  def click_element_gallery_slider_section_right_arrow(self):
+    button = self.driver.find_element(By.XPATH, '/html/body/main/div[1]/div[4]/div/div/div/ul[1]/button[2]')
+    button.click()
+    WebDriverWait(self.driver, timeout=5).until(EC.element_to_be_clickable(button))
+  
+  def get_element_gallery_slider_section_slide(self, number):
+    return self.driver.find_element(By.XPATH, f'/html/body/main/div[1]/div[4]/div/div/div/ul[1]/div/div/li[{number + 1}]')
+
+  def get_element_gallery_slider_section_current_slick_slide(self):
+    return self.driver.find_element(By.XPATH, f'/html/body/main/div[1]/div[4]/div/div/div/ul[1]/div/div//li[contains(@class, "slick-active") and contains(@class, "slick-current")]')
+  
+  def get_element_gallery_slider_current_slide_image(self):
+    current_slide = self.get_element_gallery_slider_section_current_slick_slide()
+    return current_slide.find_element(By.TAG_NAME, 'img')
+  
+  def click_element_gallery_slider_top_nav(self, number):
+    self.driver.find_element(By.XPATH, f'/html/body/main/div[1]/div[4]/div/div/div/div/ul/li[{number}]/a').click()
+
+  def get_elements_gallery_section_icons_on_current_slide(self):
+    return self.driver.find_elements(By.XPATH, "/html/body/main/div[1]/div[4]/div/div/div/ul[2]//li[contains(@style, 'list-item')]")
+  
+  def check_icons_gallery_section_current_slide_are_clickable(self):
+    icons = self.get_elements_gallery_section_icons_on_current_slide()
+    final_bool = False
+    for i in range(len(icons)):
+      bool = False
+      current_icon = icons[i]
+      bool = WebDriverWait(self.driver, timeout=3).until(EC.element_to_be_clickable(current_icon))
+      if bool:
+        final_bool = True
+      else:
+        final_bool = False
+        break
+    return final_bool
+  
+  def get_text_gallery_slider_section_footer(self):
+    return self.driver.find_element(By.XPATH, '/html/body/main/div[1]/div[4]/div/div/div/ul[2]/h5').text
+  
+  def get_element_aside_disclaimer(self):
+    return self.driver.find_elements(By.XPATH, "/html/body/main/aside[1]/div/div/aside//p")
+
+  def get_element_article_2_confidence_builder(self):
+    return self.driver.find_element(By.XPATH, '/html/body/main/article[2]')
+  
+  def get_text_article_2_header(self):
+    article_2 = self.get_element_article_2_confidence_builder()
+    return self.get_text_section_header(article_2)
+  
+  def get_text_article_2_body(self):
+    article_2 = self.get_element_article_2_confidence_builder()
+    return self.get_text_section_body(article_2)
+  
+  def get_element_article_2_image(self):
+    article_2 = self.get_element_article_2_confidence_builder()
+    return self.get_element_section_placeholder_image(article_2)
+
+  def click_element_article_2_button(self):
+    article_2 = self.get_element_article_2_confidence_builder()
+    return self.click_element_section_button(article_2)
+  
--- test_energy_efficiency_page.py
+++ test_energy_efficiency_page.py
@@ -15,8 +15,8 @@ class Test_Energy_Efficiency_Page(BasicTest):
   @pytest.fixture()
   def driver_settings(self):
     self.driver.get(URL)
-    self.driver.set_window_position(0,0)
-    self.driver.set_window_size(1920,1080)
+    # self.driver.set_window_position(0,0)
+    # self.driver.set_window_size(1920,1080)
   
   # Test that youtube overlay pop up after clicking play button
   def test_overlay_on_play_button_click(self, driver_settings):
@@ -151,11 +151,139 @@ class Test_Energy_Efficiency_Page(BasicTest):
       case '3':
         assert 'PEACE OF MIND' in header and 'AN INDUSTRY' in sub_header and 'been building better for more than' in body
   
-  @pytest.mark.energy
-  def test_donut_slides_buttons(self, driver_settings):
+  # Test that clicking the buttons in each slide opens the respective popups
+  @pytest.mark.parametrize('slide', ['1', '2', '3', '4'])
+  def test_donut_slides_buttons(self, slide, driver_settings):
     energy_page = EnergyEfficiencyPage(self.driver)
     energy_page.close_cookies()
-    buttons = energy_page.get_elements_donut_current_slide_buttons()
-    assert len(buttons) == 4
-
     
+    for i in range(int(slide) - 1):
+      energy_page.click_element_donut_section_right_arrow()
+      time.sleep(1)
+    
+    time.sleep(0.5)
+
+    final_result = False
+    for i in range(4): 
+      energy_page.click_element_current_slide_button(i+1)
+      popup = energy_page.get_element_current_slide_button_popup(i+1)
+      result = self.driver.execute_script("return arguments[0].style.display != 'none'", popup)
+      if result:
+        final_result = True
+        energy_page.click_element_empty_space()
+        time.sleep(1)
+        continue
+      else:
+        final_result = False
+        break
+    
+    assert final_result
+  
+  # Test whether the correct header is displayed in the house drawing icons section
+  def test_house_drawing_section_header(self):
+    energy_page = EnergyEfficiencyPage(self.driver)
+    header = energy_page.get_text_house_icon_section_header()
+    assert "Click on any icon or scroll through" in header
+  
+  # Test whether the correct image is displayed in the house drawing icons section
+  def test_house_drawing_section_image(self):
+    energy_page = EnergyEfficiencyPage(self.driver)
+    image = energy_page.get_element_house_icon_section_image()
+    result = self.driver.execute_script("return arguments[0].complete && " + "arguments[0].width > 0", image)
+    assert result
+
+  # Test that the buttons on the house image are all clickable
+  def test_house_icons_clickable(self, driver_settings):
+    energy_page = EnergyEfficiencyPage(self.driver)
+    icons = energy_page.get_elements_house_icons()
+    final_result = False
+    number_icons = 0
+    for i in range(len(icons)):
+      print("i = ", i)
+      is_clickable = energy_page.check_icons_is_clickable(i+1)
+      if is_clickable:
+        final_result = True
+        number_icons = i + 1
+        continue
+      else:
+        final_result = False
+        break
+      
+    assert final_result and number_icons == 32
+  
+  # Test whether correct header appears for the gallery slider section
+  def test_gallery_header(self, driver_settings):
+    energy_page = EnergyEfficiencyPage(self.driver)
+    header = energy_page.get_text_gallery_slider_section_header()
+    assert "Slide through the gallery below" in header
+  
+  # Test whether correct sub_header appears for the gallery slider section
+  def test_gallery_sub_header(self, driver_settings):
+    energy_page = EnergyEfficiencyPage(self.driver)
+    sub_header = energy_page.get_text_gallery_slider_section_sub_header()
+    assert "Click any" in sub_header
+  
+  # Test the navigation buttons on the top of the gallery slide container and ensure they anvigate to correct slide when pressed
+  @pytest.mark.parametrize('nav_number', ['1', '2', '3', '4', '5'])
+  def test_gallery_top_nav(self, nav_number, driver_settings):
+    energy_page = EnergyEfficiencyPage(self.driver)
+    energy_page.close_cookies()
+    energy_page.click_element_gallery_slider_top_nav(int(nav_number))
+    time.sleep(.5)
+    expected_slide = energy_page.get_element_gallery_slider_section_slide(int(nav_number))
+    result = self.driver.execute_script("return arguments[0].classList.contains('slick-current') && " + "arguments[0].classList.contains('slick-active')", expected_slide)
+    assert result
+
+  # Test whether all of the icons apper on their respective slides in the gallery
+  @pytest.mark.parametrize('slide', ['0', '1', '2', '3', '4'])
+  def test_icons_in_slides_are_clickable(self, slide, driver_settings):
+    energy_page = EnergyEfficiencyPage(self.driver)
+    energy_page.close_cookies()
+    for i in range(int(slide)):
+      energy_page.click_element_gallery_slider_section_right_arrow()
+      time.sleep(1)
+    clickable = energy_page.check_icons_gallery_section_current_slide_are_clickable()
+    assert clickable
+  
+  # Test whether the correct footer text is appearing in the gallery section
+  def test_gallery_section_footer(self):
+    energy_page = EnergyEfficiencyPage(self.driver)
+    footer = energy_page.get_text_gallery_slider_section_footer()
+    assert "M.Connected Home" in footer
+  
+  # Test whether the correct text is appearing in the gallery section disclaimer
+  def test_aside_disclaimer_text(self, driver_settings):
+    energy_page = EnergyEfficiencyPage(self.driver)
+    disclaimer = energy_page.get_element_aside_disclaimer()
+    assert (
+      "For additional legal" in disclaimer[0].text and
+      "All of our homes built in California" in disclaimer[1].text and
+      "Picture is representative and may depict or contain" in disclaimer[2].text
+    )
+  
+  # Test whether the correct header appears in article 2 a real confidence builder section
+  def test_article_2_header(self, driver_settings):
+    energy_page = EnergyEfficiencyPage(self.driver)
+    header = energy_page.get_text_article_2_header()
+    assert "A real confidence builder" in header
+
+  # Test whether the correct body appears in article 2 a real confidence builder section
+  def test_article_2_body(self, driver_settings):
+    energy_page = EnergyEfficiencyPage(self.driver)
+    body = energy_page.get_text_article_2_body()
+    assert "A real confidence builder" in body
+
+  # Test whether the correct image appears in article 2 a real confidence builder section
+  def test_article_2_image(self, driver_settings):
+    energy_page = EnergyEfficiencyPage(self.driver)
+    image = energy_page.get_element_article_2_image()
+    result = self.driver.execute_script("return arguments[0].complete && " + "arguments[0].width > 0", image)
+    assert result
+  
+  # Test whether the button in the a real confidence builder sections takes the usert ot the correct page when clicked
+  def test_article_2_image(self, driver_settings):
+    energy_page = EnergyEfficiencyPage(self.driver)
+    energy_page.click_element_article_2_button()
+    assert "Meritage Homes Awards and Accolades | Meritage Homes" == self.driver.title
+
+# Up to HERS
