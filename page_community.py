4e0414525d3acf4391313360637db4c9caab8bfc
LoganF-1234 <logan.ferrera@meritagehomes.com>
2022-10-03 13:13:31 -0700

    Begin community page tests

--- page_community.py
+++ page_community.py
@@ -6,7 +6,7 @@ from page_base import BasePage
 
 class CommunityPage(BasePage):
   def get_element_image_carousel(self):
-    return self.driver.find_element(By.ID, 'Orchard and Woodland')
+    return self.driver.find_element(By.XPATH, ".//div[contains(@class,'carousel-hero')]")
   
   def click_element_next_arrow(self):
     carousel_section = self.get_element_image_carousel()
@@ -14,12 +14,17 @@ class CommunityPage(BasePage):
   
   def get_elements_carousel_slides(self):
     carousel_section = self.get_element_image_carousel()
-    slides = carousel_section.find_elements(By.XPATH, ".//li[contains(@class, 'slick-slide') and not contains(@class, 'slick-cloned')]")
+    slides = carousel_section.find_elements(By.XPATH, ".//li[contains(@class, 'slick-slide') and not(contains(@class, 'slick-cloned'))]")
     return slides
   
   def get_element_current_slide(self):
     carousel_section = self.get_element_image_carousel()
     return carousel_section.find_element(By.XPATH, ".//li[contains(@class, 'is-active')]")
+  
+  def get_element_slide_number(self, number):
+    carousel_section = self.get_element_image_carousel()
+    slides = self.get_elements_carousel_slides()
+    return slides[number]
   
   def get_element_compare(self):
     carousel_section = self.get_element_image_carousel()
@@ -53,13 +58,13 @@ class CommunityPage(BasePage):
   def get_element_section_tabs(self):
     return self.driver.find_element(By.CLASS_NAME, 'tabbed-accordion    ')
   
-  def get_element_tabs_header(self):
+  def get_text_tabs_header(self):
     tabs_section = self.get_element_section_tabs()
-    return tabs_section.find_element(By.TAG_NAME, 'h2')
+    return tabs_section.find_element(By.TAG_NAME, 'h2').text
   
-  def get_element_tabs_body(self):
+  def get_text_tabs_body(self):
     tabs_section = self.get_element_section_tabs()
-    return tabs_section.find_element(By.TAG_NAME, 'p')
+    return tabs_section.find_element(By.TAG_NAME, 'p').text
   
   def get_elements_tabs(self):
     tabs_section = self.get_element_section_tabs()
@@ -69,6 +74,7 @@ class CommunityPage(BasePage):
   def get_element_tab_image(self, number):
     tabs = self.get_elements_tabs()
     return tabs[number].find_element(By.TAG_NAME, 'img')
+    # test with naturalWidth not width since width includes width if alt text is displayed
   
   def get_text_tab_name(self, number):
     tabs = self.get_elements_tabs()
@@ -80,7 +86,7 @@ class CommunityPage(BasePage):
 
   def get_element_active_info_tab(self, number):
     tabs_section = self.get_element_section_tabs()
-    info_tab_container = tabs_section.find_element(By.CLASS_NAME, 'responsive-tabstabs-content')
+    info_tab_container = tabs_section.find_element(By.CLASS_NAME, 'responsive-tabs.tabs-content')
     return info_tab_container.find_element(By.XPATH, f".//li[{number}]")
   
   def get_element_info_tab_overview(self):
@@ -100,5 +106,131 @@ class CommunityPage(BasePage):
     return overview_info_tab.find_element(By.XPATH, './/div/section/div/div/div[2]/div/div[1]/p[2]').text
     #assert string is not empty
 
+  def get_text_hoa(self):
+    overview_info_tab = self.get_element_info_tab_overview()
+    return overview_info_tab.find_element(By.XPATH, './/div/section/div/div/div[2]/div/div[1]/div[1]/div[1]/strong')
   
+  def get_text_tax_rate(self):
+    overview_info_tab = self.get_element_info_tab_overview()
+    return overview_info_tab.find_element(By.XPATH, './/div/section/div/div/div[2]/div/div[1]/div[1]/div[2]/strong')
+  
+  def get_text_hoa_dynamic(self):
+    overview_info_tab = self.get_element_info_tab_overview()
+    return overview_info_tab.find_element(By.XPATH, './/div/section/div/div/div[2]/div/div[1]/div[2]/div[1]/p')
+  
+  def get_text_tax_rate_dynamic(self):
+    overview_info_tab = self.get_element_info_tab_overview()
+    return overview_info_tab.find_element(By.XPATH, './/div/section/div/div/div[2]/div/div[1]/div[2]/div[2]/p')
+  
+  def get_text_sales_counselors(self):
+    overview_info_tab = self.get_element_info_tab_overview()
+    return overview_info_tab.find_element(By.XPATH, './/div/section/div/div/div[2]/div/div[2]/strong')
+  
+  def get_element_info_tab_our_floorplans(self):
+    return self.get_element_active_info_tab(2)
+  
+  def get_elements_floorplans(self):
+    floorplans_info_tab = self.get_element_info_tab_our_floorplans()
+    floorplans_container = floorplans_info_tab.find_element(By.XPATH, './/div/section/div[2]')
+    return floorplans_container.find_elements(By.XPATH, './div')
+  
+  def get_element_floorplan_image(self, number):
+    floorplans = self.get_elements_floorplans()
+    return floorplans[number].find_element(By.TAG_NAME, 'img')
+  
+  def get_text_floorplan_snipe_overlay(self, number):
+    floorplans = self.get_elements_floorplans()
+    return floorplans[number].find_element(By.TAG_NAME, 'span').text
+    # assert != ''
+  
+  def get_element_floorplan_name(self, number):
+    floorplans = self.get_elements_floorplans()
+    return floorplans[number].find_element(By.XPATH, ".//a[contains(@href, '/state')]")
+  
+  def get_text_floorplan_name(self, number):
+    name_element = self.get_element_floorplan_name(number)
+    return name_element.text
 
+  def click_element_floorplan_name(self, number):
+    html = self.get_html()
+    name_element = self.get_element_floorplan_name(number)
+    name_element.click()
+    WebDriverWait(self.driver, timeout=3).until(EC.staleness_of(html))
+  
+  def get_element_info_tab_qmis(self):
+    return self.get_element_active_info_tab(3)
+  
+  def get_elements_qmis(self):
+    qmis_info_tab = self.get_element_info_tab_qmis()
+    qmis_container = qmis_info_tab.find_element(By.XPATH, './/div/section/div[2]')
+    return qmis_container.find_elements(By.XPATH, './div')
+  
+  def get_element_qmi_image(self, number):
+    qmis = self.get_elements_qmis()
+    return qmis[number].find_element(By.TAG_NAME, 'img')
+  
+  def get_element_qmi_name(self, number):
+    qmis = self.get_elements_qmis()
+    return qmis[number].find_element(By.XPATH, ".//a[contains(@href, '/state')]")
+  
+  def get_text_qmi_plan(self, number):
+    qmis = self.get_elements_qmis()
+    plan_number = qmis[number].find_element(By.CLASS_NAME, 'plan-number')
+    return plan_number.text
+  
+  # This is getting the element inside of the specfic QMI page and breaks the page object model.
+  def get_new_page_qmi_plan_number(self):
+    article = self.get_section_by_aria_label('article', 'overview B article')
+    return article.find_element(By.TAG_NAME, 'p').text
+
+  def click_element_qmi_name(self, number):
+    html = self.get_html()
+    name_element = self.get_element_qmi_name(number)
+    name_element.click()
+    WebDriverWait(self.driver, timeout=3).until(EC.staleness_of(html))
+
+  def get_element_info_tab_community(self):
+    return self.get_element_active_info_tab(4)
+
+  def get_elements_community_rows(self):
+    communities_info_tab = self.get_element_info_tab_community()
+    return communities_info_tab.find_elements(By.TAG_NAME, 'article')
+  
+  def get_element_row_image(self, number):
+    rows = self.get_elements_community_rows()
+    return rows[number].find_element(By.TAG_NAME, 'img')
+    
+  def get_element_info_tab_homesite(self):
+    return self.get_element_active_info_tab(5)
+  
+  def check_clickable_homesite_button(self):
+    homesite_info_tab = self.get_element_info_tab_homesite()
+    button = homesite_info_tab.find_element(By.XPATH, './/div/section/a')
+    return self.button_is_clickable(button)
+  
+  def get_element_iframe(self):
+    homesite_info_tab = self.get_element_info_tab_homesite()
+    return homesite_info_tab.find_element(By.XPATH, './/div/section/iframe')
+
+  def get_element_info_tab_video(self):
+    return self.get_element_active_info_tab(6)
+
+  def get_element_video_iframe(self):
+    video_info_tab = self.get_element_info_tab_video()
+    return video_info_tab.find_element(By.TAG_NAME, 'iframe')
+
+  def get_aside_stay_up_to_date(self):
+    return self.get_section_by_aria_label('aside', 'Interest list sign up form')
+  
+  def click_element_homebuyer_button(self):
+    stay_section = self.get_aside_stay_up_to_date()
+    stay_section.find_element(By.XPATH, './/div[1]/div/form/div[1]/div/div[1]/label')
+  
+  def click_element_agent_button(self):
+    stay_section = self.get_aside_stay_up_to_date()
+    stay_section.find_element(By.XPATH, './/div[1]/div/form/div[1]/div/div[2]/label')
+  
+  def check_aside_button_clickable(self):
+    stay_section = self.get_aside_stay_up_to_date()
+    button = stay_section.find_element(By.XPATH, './/div[1]/div/form/div[3]/div[4]/button')
+    return self.button_is_clickable(button)
--- test_community_page.py
+++ test_community_page.py
@@ -9,9 +9,231 @@ import time
 
 @pytest.mark.usefixtures("init__driver")
 class BasicTest():
-  def test_city_dropdown(self, driver_settings):
+  def test_image_carousel(self, driver_settings):
     community_page = CommunityPage(self.driver)
+    community_page.close_cookies()
+    slides = community_page.get_elements_carousel_slides()
+    carousel_bool = False
+    for i in range(len(slides)):
+      if i != 0:
+        community_page.click_element_next_arrow()
+        time.sleep(1)
+      slide = community_page.get_element_slide_number(i)
+      result = self.driver.execute_script("return arguments[0].classList.contains('slick-active')", slide)
+      if result:
+        carousel_bool = True
+      else:
+        assert False
+    assert carousel_bool 
+  
+  def test_compare_clickable(self, driver_settings):
+    community_page = CommunityPage(self.driver)
+    compare_button = community_page.get_element_compare()
+    assert community_page.button_is_clickable(compare_button)
+  
+  def test_heart_clickable(self, driver_settings):
+    community_page = CommunityPage(self.driver)
+    heart_button = community_page.get_element_heart()
+    assert community_page.button_is_clickable(heart_button)
+  
+  def test_header(self, driver_settings):
+    community_page = CommunityPage(self.driver)
+    header = community_page.get_text_main_header()
+    assert '' != header
     
+  def test_sub_header(self, driver_settings):
+    community_page = CommunityPage(self.driver)
+    sub_header = community_page.get_text_main_body()
+    assert '' != sub_header
+  
+  def test_community_info(self, driver_settings):
+    community_page = CommunityPage(self.driver)
+    info = community_page.get_text_aside()
+    assert (
+      'Approx. Sq. Ft.' in info and
+      'Bedrooms' in info and
+      'Garage' in info and
+      'Stories' in info
+    )
+  
+  def test_brochure_clickable(self, driver_settings):
+    community_page = CommunityPage(self.driver)
+    brochure = community_page.get_element_brochure()
+    assert community_page.button_is_clickable(brochure)
+  
+  def test_tabs_header(self, driver_settings):
+    community_page = CommunityPage(self.driver)
+    header = community_page.get_text_tabs_header()
+    assert 'Explore what makes' in header
+  
+  def test_tabs_body(self, driver_settings):
+    community_page = CommunityPage(self.driver)
+    body = community_page.get_text_tabs_body()
+    assert 'See why residents' in body
+  
+  def test_tab_images(self, driver_settings):
+    community_page = CommunityPage(self.driver)
+    tabs = community_page.get_elements_tabs()
+    images_bool = False
+    for i in range(len(tabs)):
+      tab_image = community_page.get_element_tab_image(i)
+      result = self.driver.execute_script("return arguments[0].complete && " + "arguments[0].naturalWidth > 0", tab_image)
+      if result:
+        images_bool = True
+        continue
+      else:
+        assert False
+    assert images_bool
+  
+  def test_tabs_text(self, driver_settings):
+    community_page = CommunityPage(self.driver)
+    tabs = community_page.get_elements_tabs()
+    text_bool = False
+    for i in range(len(tabs)):
+      tab_name = community_page.get_text_tab_name(i)
+      match i:
+        case 0:
+          if 'Overview' in tab_name:
+            text_bool = True
+          else:
+            assert False
+        case 1:
+          if 'Our Floorplans' in tab_name:
+            text_bool = True
+          else:
+            assert False
+        case 2:
+          if 'Quick Move' in tab_name:
+            text_bool = True
+          else:
+            assert False
+        case 3:
+          if 'Community' in tab_name:
+            text_bool = True
+          else:
+            assert False
+        case 4:
+          if 'Find a Homesite' in tab_name:
+            text_bool = True
+          else:
+            assert False
+        case 5:
+          if 'Community Video' in tab_name:
+            text_bool = True
+          else:
+            assert False
+    assert text_bool
+  
+  # Some communities do not contain the approximate hoa or approximate tax rates elements. Need to include that in the test below.
+  def test_tab_overview(self, driver_settings):
+    community_page = CommunityPage(self.driver)
+    community_page.close_cookies()
+    community_address = community_page.get_text_community_address()
+    directions_link = community_page.get_element_directions_link()
+    body = community_page.get_text_below_directions()
+    hoa_header = community_page.get_text_hoa()
+    tax_header = community_page.get_text_tax_rate()
+    hoa_dynamic = community_page.get_text_hoa_dynamic()
+    tax_dynamic = community_page.get_text_tax_rate_dynamic()
+    assert (
+      len(community_address) > 2 and
+      community_page.check_plain_button(directions_link) and
+      '' != body and
+      'Approximate Monthly' in hoa_header and
+      '' != hoa_dynamic and
+      'Approximate Tax' in tax_header and
+      '' != tax_dynamic
+    )
+  
+  def test_tab_our_floorplans(self, driver_settings):
+    community_page = CommunityPage(self.driver)
+    community_page.close_cookies()
+    community_page.click_element_tab(1)
+    floorplan_elements = community_page.get_elements_floorplans()
+    all_images_present = False
+    for i in range(len(floorplan_elements)):
+      image = community_page.get_element_floorplan_image(i)
+      result = community_page.javascript_image(image)
+      if result:
+        all_images_present = True
+      else:
+        assert False
+    for i in range(len(floorplan_elements)):
+      overlay_text = community_page.get_text_floorplan_snipe_overlay(i)
+      if overlay_text != '':
+        all_overlays_present = True
+      else:
+        assert False
+      
+    name = community_page.get_text_floorplan_name(0)
+    community_page.click_element_floorplan_name(0)
+      
+    assert (
+      len(floorplan_elements) > 0 and
+      all_images_present and
+      all_overlays_present and 
+      name in self.driver.title
+    )
+  
+  # some pages are supposed to have no qmis how do I fix this test to include that case
+  def test_tab_qmis(self, driver_settings):
+    community_page = CommunityPage(self.driver)
+    community_page.close_cookies()
+    community_page.click_element_tab(2)
+    qmi_elements = community_page.get_elements_qmis()
+    all_images_present = False
+    for i in range(len(qmi_elements)):
+      image = community_page.get_element_qmi_image(i)
+      result = community_page.javascript_image(image)
+      if result:
+        all_images_present = True
+      else:
+        assert False
+    
+    container_plan_number = community_page.get_text_qmi_plan(0)
+    community_page.click_element_qmi_name(0)
+    new_page_plan_number = community_page.get_new_page_qmi_plan_number()
+
+    assert (
+      len(qmi_elements) > 0 and
+      all_images_present and
+      container_plan_number.lower() == new_page_plan_number().lower()
+    )
+  
+  def test_tab_community(self, driver_settings):
+    community_page = CommunityPage(self.driver)
+    community_page.close_cookies()
+    community_page.click_element_tab(3)
+    rows = community_page.get_elements_community_rows()
+    image_bool = False
+    for i in range(len(rows)):
+      image = community_page.get_element_row_image(i)
+      result = community_page.javascript_image(image)
+      if result:
+        image_bool = True
+      else:
+        assert False
+    assert image_bool
+  
+  # Some of these tabs do not have th iframe, need to include that case in the below test
+  def test_tab_homesite(self, driver_settings):
+    community_page = CommunityPage(self.driver)
+    community_page.close_cookies()
+    community_page.click_element_tab(4)
+    button_clickable = community_page.check_clickable_homesite_button()
+    iframe = community_page.get_element_iframe()
+    result = self.driver.execute_script("return arguments[0].width > 0", iframe)
+    assert button_clickable and result
+  
+  def test_tab_video(self, driver_settings):
+    community_page = CommunityPage(self.driver)
+    community_page.close_cookies()
+    community_page.click_element_tab(5)
+    iframe = community_page.get_element_video_iframe()
+    result = self.driver.execute_script("return arguments[0].width == '100%'", iframe)
+    assert result
+    
+
   pass
 
 class Test_Denver_Horizon_Uptown_Page(BasicTest):
